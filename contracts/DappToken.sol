cf9a20c6fe41b88bb1c876f0da39d7c88b92894d
xianhaicao <xianhaic@gmail.com>
2018-07-28 18:25:56 -0700

    added delegated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,18 +1,29 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
+    // member vars
     string public name = "DApp Token";
     string public symbol = "DAPP";
     string public standard = "DApp Token v1.0";
     uint256 public totalSupply;    
-    mapping(address => uint256) public balanceOf;   
 
+    // mappings
+    mapping(address => uint256) public balanceOf;   
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // events
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
     );
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
+    // constructor
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;    
@@ -31,4 +42,36 @@ contract DappToken {
 
         return true;
     }
+    
+    function approve(address _spender, uint256 _value) 
+        public returns (bool success) {
+        
+        // allowance
+        allowance[msg.sender][_spender] = _value;
+        // approve event
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) 
+        public returns (bool success) {
+
+        // require _from has enough tokens
+        require(balanceOf[_from] >= _value);
+
+        // require allowance is big enough 
+        require(allowance[_from][msg.sender] >= _value);
+
+        // change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // update the allowance
+        allowance[_from][msg.sender] -= _value;
+        // transfer event        
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
